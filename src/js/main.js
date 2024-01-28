1f5bb87550567227674488c6dcfe748cc5997ba5
DanielCachro <danielcachro@gmail.com>
2024-01-28 18:07:25 +0100

    Added mobile and desktop display of main container

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const hamburger = document.querySelector('.c-hamburger')
 	const navPanelMobile = document.querySelector('.c-nav__panel')
+	const body = document.querySelector('body')
 
 	hamburger.addEventListener('click', e => {
 		navPanelMobile.classList.toggle('c-nav__panel--opened')
+		body.classList.toggle('u-overflow-hidden')
 
 		const targetButton = e.target.closest('button')
 
