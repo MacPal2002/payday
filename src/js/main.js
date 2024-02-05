446f1ee25827a8f9219a2aad3d8170a04ad509d0
DanielCachro <danielcachro@gmail.com>
2024-02-05 16:52:44 +0100

    Mobile nav improvements

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Declarations - mobile nav
 	const hamburger = document.querySelector('.c-hamburger')
 	const navPanelMobile = document.querySelector('.c-nav__panel')
-	const body = document.querySelector('body')
 
 	// Mobile nav
-
-	hamburger.addEventListener('click', e => {
-		navPanelMobile.classList.toggle('c-nav__panel--opened')
-		body.classList.toggle('u-overflow-hidden')
-
-		const targetButton = e.target.closest('button')
-
-		targetButton.classList.toggle('h-opened')
-		targetButton.classList.toggle('h-gototop')
-		// navMobile.setAttribute('aria-expanded', navMobile.classList.contains('nav-mobile--opened'))
-	})
+	hamburger.addEventListener('click', () => navPanelMobile.classList.toggle('c-nav__panel--opened'))
 
 	// Slick
 	const cards = $('.l-cards')
-	const mediaQuery = window.matchMedia('(max-width: 992px)')
+	const mediaQuery = window.matchMedia('(max-width: 880px)')
 
 	const handleSwitchSlick = e => {
 		if (e.matches) {
